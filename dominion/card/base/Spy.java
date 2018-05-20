b2b8f12495d5ae260111a3390444b3f7c5ec34e9
unknown <Bapg66@hotmail.fr>
2018-05-20 18:23:25 +0200

    Correction Spy

--- dominion/card/base/Spy.java
+++ dominion/card/base/Spy.java
@@ -22,21 +22,21 @@ public class Spy extends AttackCard {
 		p.incrementActions(1);
 		Card c=p.drawCard();
 		System.out.println(c.toString());
-		String choice=p.choose("Voulez vous défausser ou replacer sur le deck la carte:"+ c.toString()+"? (d/r)", Arrays.asList("d", "r"), false);
-		if(choice=="d"){
+		String choice=p.choose("Voulez vous défausser (y) ou replacer (n) sur le deck la carte:"+ c.toString()+" ?", Arrays.asList("y", "n"), false);
+		if(choice=="y"){
 			p.getDiscard().add(c);
 		}
-		else if (choice=="r"){
+		else if (choice=="n"){
 			p.getDraw().add(0, c);
 		}
 		
 		for(Player loop_p : p.otherPlayers()){
 			c=loop_p.drawCard();
-			choice=p.choose("Voulez vous défausser ou replacer sur le deck la carte:"+ c.toString()+"du joueur"+loop_p.toString()+ "? (d/r)", Arrays.asList("d", "r"), false);
-			if(choice=="d"){
+			choice=p.choose("Voulez vous défausser(y) ou replacer(n) sur le deck la carte:"+ c.toString()+" du joueur "+loop_p.toString()+ " ?", Arrays.asList("y", "n"), false);
+			if(choice=="y"){
 				loop_p.getDiscard().add(c);
 			}
-			else if (choice=="r"){
+			else if (choice=="n"){
 				loop_p.getDraw().add(0, c);
 			}
 		}
